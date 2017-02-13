f6e7daa6dc3f4b0dd82f32a5496452c2a5822d8f
Bill <wkernan@gmail.com>
2017-02-12 21:21:16 -0600

    Fix line_item quantities to display in carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
 	# dependent: :destroy means if we destroy a cart, we also will destroy all line_items associated with it
 	has_many :line_items, dependent: :destroy
+
+	def add_product(product)
+		current_item = line_items.find_by(product_id: product.id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product.id)
+		end
+		current_item
+	end
 end
