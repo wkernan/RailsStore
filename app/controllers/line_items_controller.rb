42b8ad720e0fda7da99d728efca1b8a2faa8d66e
Bill <wkernan@gmail.com>
2017-03-04 07:43:54 -0600

    Added ajax functionality to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_index_url, notice: "#{@line_item.product.title} was added. Total price #{helpers.number_to_currency(@cart.total_price)}" }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
         session[:counter] = 0
       else
@@ -64,7 +65,7 @@ class LineItemsController < ApplicationController
       @line_item.destroy
     end
     respond_to do |format|
-      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to store_index_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -78,5 +79,9 @@ class LineItemsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
       params.require(:line_item).permit(:product_id)
+    end
+
+    def helpers
+      ActionController::Base.helpers
     end
 end
