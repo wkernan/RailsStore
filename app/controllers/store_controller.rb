12ed55d8715168f5e23dc4fcfd146889e9b5fe88
Bill <wkernan@gmail.com>
2017-03-06 20:34:29 -0600

    Action Cable to update catalog on product change

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,11 +4,5 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
 
-  	def counter
-  		session[:counter] ||= 0
-  		session[:counter] += 1
-  	end
-
-  	@counter = counter
   end
 end
